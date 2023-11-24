e640850fb967b0b5fc864534c344c5e8dc27f4ea
spoleschiNB <spoleschi@gmail.com>
2023-11-24 18:13:28 -0300

    Correcion ItemCount

--- src/components/ItemCount/ItemCount.js
+++ src/components/ItemCount/ItemCount.js
@@ -6,7 +6,7 @@ function ItemCount ({ stock, onAdd }){
   const [count, setCount] = useState(1); 
   return(    
     <div className="itemCount mx-auto">
-      <div  iv className="contador">
+      <div className="contador">
         <button onClick={() => {if(count>1) setCount(count-1) }}> - </button>
         <div> {count} </div>
         <button onClick={() => {if(count < stock) setCount(count+1)}}> + </button>
