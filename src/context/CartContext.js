5b4fe62381daa0d6b2bec0541121e278560719e6
spoleschiNB <spoleschi@gmail.com>
2023-11-24 23:42:12 -0300

    Contexto de carrito y carWidget

--- src/context/CartContext.js
+++ src/context/CartContext.js
@@ -1,15 +1,23 @@
 import { React, useState, createContext } from 'react'
 
-export const CartContext = createContext();
+export const CartContext = createContext({
+	cart: 0,
+	cantTot: 0,
+});
 
 export const CartProvider = ( {children} ) => {
 
 	const [cart, setCart] = useState([]);
+	const [cantTot, setCantTot] = useState(0);
 	console.log(cart);
 
 	const addItem = (productToAdd) => {
 		console.log(productToAdd)
-		if (!isInCart(productToAdd.id))  setCart ([...cart,productToAdd]);
+		if (!isInCart(productToAdd.id)){
+			setCart ([...cart,productToAdd]);
+			setCantTot(cantTot+Number(productToAdd.cant));
+		}
+			
 		else console.log('Ya se agregó ese producto previamente');
 	}
 
@@ -17,8 +25,13 @@ export const CartProvider = ( {children} ) => {
 		return cart.some(prod => prod.id === id);
 	} 
 	
+	// const removeProduct = (id) => {
+	// 	const cartNew = cart.filter(prod => prod.id !== id);
+	// 	setCart(cartNew);
+	// }
+
 	return (
-		<CartContext.Provider value={{ cart, addItem }}>
+		<CartContext.Provider value={{ cart, addItem, cantTot }}>
 			{children}
 		</CartContext.Provider>
 	)
